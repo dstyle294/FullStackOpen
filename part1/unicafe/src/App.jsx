7744a0363780d559cbc697198b9726bbc91cc03d
Sahil Dalal <skdalal@ucsd.com>
2025-06-22 01:15:01 +0530

    1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,17 +1,31 @@
 import { useState } from 'react'
 
+const StatisticLine = (props) => {
+  if (props.text != 'positive') {
+    return (
+      <div>
+        {props.text} {props.value}
+      </div>
+    )
+  }
+  return (
+    <div>
+      {props.text} {props.value} %
+    </div>
+  )
+}
 
 const Statistics = (props) => {
   if (props.total != 0) {
     return (
       <div>
         <h1>statistics</h1>
-        good {props.good} <br />
-        neutral {props.neutral} <br />
-        bad {props.bad} <br />
-        total {props.total} <br />
-        average {(props.good - props.bad) / props.total} <br />
-        positive {(props.good * 100) / props.total} %
+        <StatisticLine text = "good" value = {props.good} />
+        <StatisticLine text = "neutral" value = {props.neutral} />
+        <StatisticLine text = "bad" value = {props.bad} />
+        <StatisticLine text = "total" value = {props.total} />
+        <StatisticLine text = "average" value = {(props.good - props.bad) / props.total} />
+        <StatisticLine text = "positive" value = {(props.good * 100) / props.total} /> 
       </div>
     )
   }
@@ -27,6 +41,10 @@ const Statistics = (props) => {
     </div>
   )
   
+}
+
+const Button = (props) => {
+  return <button onClick = {props.handler}>{props.text}</button>
 }
 
 const App = () => {
@@ -52,13 +70,15 @@ const App = () => {
     setTotal(total + 1)
   }
 
+  
+
   if (total == 0) {
     return (
       <div>
         <h1>give feedback</h1>
-        <button onClick={handleGood}>good</button>
-        <button onClick={handleNeutral}>neutral</button>
-        <button onClick={handleBad}>bad</button>
+        <Button handler={handleGood} text = 'good' />
+        <Button handler={handleNeutral} text = 'neutral' />
+        <Button handler={handleBad} text = 'bad' />
         <h1>statistics</h1>
         <p>No feedback given</p>
       </div>
