bc539bd0265c6a6d6918b07bb7ea6932794d6dae
Sahil Dalal <skdalal@ucsd.com>
2025-06-28 10:19:34 +0530

    part 2 done

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,13 +1,108 @@
 import { useState, useEffect } from 'react'
 import countriesService from './services/countries'
 
-const SingleCountry = ({name, countries}) => {
-  console.log(name)
-  const countryObj = countries.find((country) => {
+// const SingleCountry = ({name, countries}) => {
+//   const [weather, setWeather] = useState(null)
 
+//   const countryObj = countries.find((country) => {
+//     if (country.name.common === name) return country
+//   })
+
+  
+
+//   console.log(countryObj)
+
+//   if (!countryObj || !countryObj.capital) return null
+
+//   const capital = Array.isArray(countryObj.capital) ? countryObj.capital[0] : countryObj.capital
+//   console.log("About to enter useEffect for", capital)
+
+//   useEffect(() =>{
+//     countriesService
+//       .getTemp(capital)
+//     .then(
+//       weather => {
+//         setWeather(weather)
+//       })
+//     }, [capital])
+
+  
+
+//   console.log(Object.values(countryObj.languages))
+//   console.log(capital)
+
+
+  
+
+  
+  
+//   return (
+//     <div>
+//       <h1>
+//         {countryObj.name.common}
+//       </h1>
+//       <p>
+//         Capital {countryObj.capital} <br />
+//         Area {countryObj.area}  
+//       </p>
+//       <h2>
+//         Languages
+//       </h2>
+//       <ul>
+//         {Object.values(countryObj.languages).map(
+//           (language) => {
+//             return <li>{language}</li>
+//           }
+//         )}
+//       </ul>
+//       <img src={countryObj.flags.png} alt={'Country flag'} style={{width:'300px'}}/>
+//       <h2>
+//         Weather in {capital}
+//       </h2>
+//       Temperature {weather.main.temp} Celsius <br />
+//       <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={'weather icon'} />
+//       Wind {weather.wind.speed} m/s
+//     </div>
+//   )
+// }
+
+const SingleCountry = ({name, countries}) => {
+  const [weather, setWeather] = useState(null)
+  const [weatherLoading, setWeatherLoading] = useState(true)
+  const [weatherError, setWeatherError] = useState(null)
+
+  const countryObj = countries.find((country) => {
     if (country.name.common === name) return country
   })
+
+  console.log(countryObj)
+
+  if (!countryObj || !countryObj.capital) return null
+
+  const capital = Array.isArray(countryObj.capital) ? countryObj.capital[0] : countryObj.capital
+  console.log("About to enter useEffect for", capital)
+
+  useEffect(() => {
+    setWeatherLoading(true)
+    setWeatherError(null)
+    
+    countriesService
+      .getTemp(capital)
+      .then(weather => {
+        console.log("Weather data received:", weather)
+        setWeather(weather)
+        setWeatherLoading(false)
+      })
+      .catch(error => {
+        console.error("Error fetching weather:", error)
+        setWeatherError(error)
+        setWeatherLoading(false)
+      })
+  }, [capital])
+
   console.log(Object.values(countryObj.languages))
+  console.log(capital)
+
   return (
     <div>
       <h1>
@@ -21,20 +116,33 @@ const SingleCountry = ({name, countries}) => {
         Languages
       </h2>
       <ul>
-        {Object.values(countryObj.languages).map(
-          (language) => {
-            return <li>{language}</li>
-          }
-        )}
+        {Object.values(countryObj.languages).map((language, index) => {
+          return <li key={index}>{language}</li>
+        })}
       </ul>
       <img src={countryObj.flags.png} alt={'Country flag'} style={{width:'300px'}}/>
+      
+      <h2>
+        Weather in {capital}
+      </h2>
+      
+      
+      {weather && !weatherLoading && (
+        <div>
+          Temperature {weather.main.temp} Celsius <br />
+          <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={'weather icon'} /> <br />
+          Wind {weather.wind.speed} m/s
+        </div>
+      )}
     </div>
   )
 }
 
-function App() {
+
+const App = () => {
   const [country, setCountry] = useState('')
   const [countries, setCountries] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState(null)
 
   useEffect(() => {
     countriesService.getAll()
@@ -43,19 +151,31 @@ function App() {
     )
   }, [])
 
+  
+
 
   const handleCountryChange = (event) => {
     setCountry(event.target.value)
+    setSelectedCountry(null)
   }
+
+  const handleClick = (country) => {
+    console.log(country)
+  }
+  
   
 
   const countries_names = countries.map((countryObject) => {
       return countryObject.name.common
   })
+
+ 
   const filteredList = countries_names.filter((thisCountry) => {
     console.log(thisCountry, country)
     return thisCountry.toLowerCase().includes(country)
   })
+
+  
 
   console.log(filteredList)
 
@@ -66,21 +186,33 @@ function App() {
         <form>
           find countries <input value={country} onChange={handleCountryChange} />
         </form>
-        <SingleCountry name={filteredList[0]} countries={countries} />
+        <SingleCountry name={filteredList[0]} countries={countries}  />
       </div>
     )
   } else if (filteredList.length <= 10) {
-    return (
-      <div>
-        <form>
-          find countries <input value={country} onChange={handleCountryChange} />
-        </form>
-        <ul>
-          {filteredList.map((item) => <div>{item}</div>)}
-        </ul>
-      </div>
-    )
-    
+    if (selectedCountry) {
+      return (
+        <div>
+          <form>
+            find countries <input value={country} onChange={handleCountryChange} />
+          </form>
+          <SingleCountry name={selectedCountry} countries={countries}  /> 
+        </div>
+      )  
+    } else {
+      return (
+        <div>
+          <form>
+            find countries <input value={country} onChange={handleCountryChange} />
+          </form>
+          <ul>
+            {filteredList.map((item) => <div>
+              {item} <button onClick={() => {setSelectedCountry(item)}}>show</button>
+            </div>)}
+          </ul>
+        </div>
+      )
+    }
   } else {
     return (
       <div>
--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -1,10 +1,17 @@
 import axios from 'axios'
 const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all'
 const specificCountryUrl = 'https://studies.cs.helsinki.fi/restcountries/api/name/'
+const api_key = import.meta.env.VITE_SOME_KEY
 
 const getAll = () => {
     const request = axios.get(baseUrl)
     return request.then(response => response.data)
 }
 
-export default { getAll }
+const getTemp = (city) => {
+    const request = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`)
+    console.log(request)
+    return request.then(response => response.data)
+} 
+
+export default { getAll, getTemp }
