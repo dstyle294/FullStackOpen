9d4aa6eb11d7006be216d15643d0cf5854680d58
Sahil Dalal <skdalal@ucsd.com>
2025-07-02 20:59:42 +0530

    _ added port safety

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,3 +1,9 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const Note = require('./models/note')
+
+
+
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -39,17 +45,15 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end() // if request is undefined, it will be here
-    }
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -74,15 +78,15 @@ app.post('/api/notes', (request, response) => {
         })
     }
     
-    const note = {
-        content: body.content, 
+    const note = new Note({
+        content: body.content,
         important: body.important || false,
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 
-    response.json(note)
 })
 
 const unknownEndPoint = (request, response) => {
